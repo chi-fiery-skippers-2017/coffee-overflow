ca5c3c8a8235827f3706364e7c11faae3a1c33ff
Gabriel Osborne <osbornegabriel@gmail.com>
2017-07-28 16:59:46 -0500

    Write skeleton for form partial of new question

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,4 +1,12 @@
+get '/questions/new' do
+  if request.xhr?
+    erb :'questions/_new.erb', layout: false
+  end
+end
+
+
 get '/questions/:id/show' do
   @question = Question.find_by(id: params[:id])
   erb :'questions/show'
 end
+
--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-
-
+  $('#add-question-button').on("click", function(){
+    alert("This button has been clicked");
+  });
+  $.ajax
 
 });
