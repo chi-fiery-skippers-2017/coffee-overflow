a6a4ece0f841a1ecc45f55b8d283818d9508dfa2
Gabriel Osborne <osbornegabriel@gmail.com>
2017-07-28 21:49:29 -0500

    Hide question button when revealing question form, complete question form with inputs

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   $('#add-question-button').on("click", function(){
     console.log("This button has been clicked");
-    var $button = $(this)
+    $button = this
     $.ajax({
       url: '/questions/new',
       method: 'get'
     }).done(function(response){
+      $($button).hide();
       $('body').append(response)
     })
   });
